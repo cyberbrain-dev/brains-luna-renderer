60db05a7afe55faf6793a0318e154c84fa363b4c
bra1ns00 <godlevskiy.is.working@gmail.com>
2025-05-03 19:21:48 +0300

    Added functions for multiplication and division by a scalar, for cross product of vectors

--- src/vector.c
+++ src/vector.c
@@ -1,7 +1,6 @@
 #include "vector.h"
 
 // Vector2 operations
-
 float vector2_length(vector2_t v)
 {
     return sqrt(v.x * v.x + v.y + v.y);
@@ -17,15 +16,27 @@ vector2_t vector2_sub(vector2_t v1, vector2_t v2)
     vector2_t result = {v1.x - v2.x, v1.y - v2.y};
     return result;
 }
-float vector2_sclpr(vector2_t v1, vector2_t v2)
+vector2_t vector2_mulscl(vector2_t v, float a)
+{
+    vector2_t result = {v.x * a, v.y * a};
+
+    return result;
+}
+vector2_t vector2_divscl(vector2_t v, float a)
+{
+    vector2_t result = {v.x / a, v.y / a};
+
+    return result;
+}
+
+
+float vector2_dotpr(vector2_t v1, vector2_t v2)
 {
     return (v1.x * v2.x) + (v1.y * v2.y); 
 }
 
 
-
 // Vector3 operations
-
 float vector3_length(vector3_t v)
 {
     return sqrt(v.x * v.x + v.y + v.y + v.z * v.z);
@@ -41,9 +52,34 @@ vector3_t vector3_sub(vector3_t v1, vector3_t v2)
     vector3_t result = {v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
     return result;
 }
-float vector3_sclpr(vector3_t v1, vector3_t v2)
+vector3_t vector3_mulscl(vector3_t v, float a)
+{
+    vector3_t result = {v.x * a, v.y * a, v.z * a};
+
+    return result;
+}
+vector3_t vector3_divscl(vector3_t v, float a)
+{
+    vector3_t result = {v.x / a, v.y / a, v.z / a};
+
+    return result;
+}
+
+float vector3_dotpr(vector3_t v1, vector3_t v2)
 {
     return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z); 
+}
+
+vector3_t vector3_crosspr(vector3_t a, vector3_t b)
+{
+    vector3_t result = 
+    {
+        (a.y * b.z) - (a.z * b.y),
+        (a.z * b.x) - (a.z * b.z),
+        (a.x * b.y) - (a.y - b.x)
+    };
+
+    return result;
 }
 
 vector3_t vector3_rotate(vector3_t v, vector3_t rotation)
--- src/vector.h
+++ src/vector.h
@@ -10,6 +10,26 @@ typedef struct
     float y;
 } vector2_t;
 
+/// @brief Returns a length of 2D-vector
+float vector2_length(vector2_t v);
+
+/// @brief Sums two 2D-vectors
+vector2_t vector2_add(vector2_t v1, vector2_t v2);
+/// @brief Subtracts two 2D-vectors
+vector2_t vector2_sub(vector2_t v1, vector2_t v2);
+/// @brief Multiplies a 2D-vector by a scalar
+/// @param v 2D-vector
+/// @param a scalar
+vector2_t vector2_mulscl(vector2_t v, float a);
+/// @brief Divides a 2D-vector by a scalar
+/// @param v 2D-vector
+/// @param a scalar
+vector2_t vector2_divscl(vector2_t v, float a);
+
+/// @brief returns the scalar product of two 2D-vectors
+float vector2_dotpr(vector2_t v1, vector2_t v2);
+
+
 /// @brief Represents a vector in a three-dimensional space
 typedef struct
 {
@@ -18,22 +38,6 @@ typedef struct
     float z;
 } vector3_t;
 
-#pragma region Vector2 operations
-
-/// @brief Returns a length of 2D-vector
-float vector2_length(vector2_t v);
-
-/// @brief Sums two 2D-vectors
-vector2_t vector2_add(vector2_t v1, vector2_t v2);
-/// @brief Subtracts two 2D-vectors
-vector2_t vector2_sub(vector2_t v1, vector2_t v2);
-/// @brief returns the scalar product of two 2D-vectors
-float vector2_sclpr(vector2_t v1, vector2_t v2);
-
-#pragma endregion
-
-#pragma region Vector3 operations
-
 /// @brief Returns a length of 3D-vector
 float vector3_length(vector3_t v);
 
@@ -41,8 +45,20 @@ float vector3_length(vector3_t v);
 vector3_t vector3_add(vector3_t v1, vector3_t v2);
 /// @brief Subtracts two 3D-vectors
 vector3_t vector3_sub(vector3_t v1, vector3_t v2);
+/// @brief Multiplies a 3D-vector by a scalar
+/// @param v 3D-vector
+/// @param a scalar
+vector3_t vector3_mulscl(vector3_t v, float a);
+/// @brief Dicides a 3D-vector by a scalar
+/// @param v 3D-vector
+/// @param a scalar
+vector3_t vector3_divscl(vector3_t v, float a);
+
 /// @brief returns the scalar product of two 3D-vectors
-float vector3_sclpr(vector3_t v1, vector3_t v2);
+float vector3_dotpr(vector3_t v1, vector3_t v2);
+
+/// @brief returns the cross product of two 3D-vectors
+vector3_t vector3_crosspr(vector3_t a, vector3_t b);
 
 /// @brief Rotating the vector by three axis
 /// @param v a vector to be rotated
